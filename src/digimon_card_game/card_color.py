c535fb5d60974843fdb35d1b32564a7fe8dc572e
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-29 17:29:11 -0300

    :sparkles: Add card color

---
+++ src/digimon_card_game/card_color.py
@@ -0,0 +1,11 @@
+from enum import StrEnum
+
+
+class CardColor(StrEnum):
+    RED = 'Red'
+    BLUE = 'Blue'
+    YELLOW = 'Yellow'
+    GREEN = 'Green'
+    BLACK = 'Black'
+    PURPLE = 'Purple'
+    WHITE = 'White'
