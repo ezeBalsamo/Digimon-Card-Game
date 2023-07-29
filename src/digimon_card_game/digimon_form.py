821e2a67320727327624bedda12e90dbbe2af3af
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-29 17:52:37 -0300

    :sparkles: Add digimon form

---
+++ src/digimon_card_game/digimon_form.py
@@ -0,0 +1,9 @@
+from enum import StrEnum
+
+
+class DigimonForm(StrEnum):
+    IN_TRAINING = 'In-Training'
+    ROOKIE = 'Rookie'
+    CHAMPION = 'Champion'
+    MEGA = 'Mega'
+    ULTIMATE = 'Ultimate'
