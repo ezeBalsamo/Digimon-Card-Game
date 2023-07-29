efc2bc78337091320b39b93cd01f3a0748b7697f
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-29 17:55:24 -0300

    :recycle: Add expected enum values assertion

---
+++ tests/assertions/assertions.py
@@ -0,0 +1,7 @@
+from enum import Enum
+from typing import Any, Type, Set
+
+
+def assert_expected_enum_values(enum_class: Type[Enum], expected_enum_values: Set[Any]):
+    enum_values = set(map(lambda enum_item: enum_item.value, list(enum_class)))
+    assert enum_values == expected_enum_values
