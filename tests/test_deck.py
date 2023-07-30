c88d8eb742d4b500818356467bff7ef80f8e45bf
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-30 17:27:35 -0300

    :recycle: Use list raises not minimum length assertion

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -1,15 +1,12 @@
 from src.digimon_card_game import Deck
+from tests.assertions import assert_list_raises_not_minimum_length
 from src.cards import koromon, shadow_wing, biyomon
 
 from pytest import raises
 
 
 def test_01_cannot_create_deck_without_cards():
-    invalid_cards = []
-    minimum_length = 1
-    with raises(ValueError) as exception_info:
-        Deck(cards=invalid_cards)
-    assert str(exception_info.value) == f"Length of 'cards' must be => {minimum_length}: {len(invalid_cards)}"
+    assert_list_raises_not_minimum_length('cards', 1, lambda invalid_cards: Deck(cards=invalid_cards))
 
 
 def test_02_instance_creation_and_accessing():
