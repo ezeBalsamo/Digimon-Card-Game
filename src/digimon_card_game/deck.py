76c822c64194bef0e0fea02bf454339190c1e2e0
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-30 19:01:19 -0300

    :recycle: Refactor can draw many cards assertion

--- src/digimon_card_game/deck.py
+++ src/digimon_card_game/deck.py
@@ -14,14 +14,14 @@ class Deck:
         return self.cards.pop(0)
 
     def draw_many(self, number_of_cards):
-        self.assert_is_not_empty()
-        assert_is_positive(number_of_cards, 'The number of cards to draw must be positive.')
         self.assert_can_draw_many(number_of_cards)
         drawn_cards = self.cards[:number_of_cards]
         del self.cards[:number_of_cards]
         return drawn_cards
 
     def assert_can_draw_many(self, number_of_cards):
+        self.assert_is_not_empty()
+        assert_is_positive(number_of_cards, 'The number of cards to draw must be positive.')
         number_of_remaining_cards = len(self.cards)
         if number_of_remaining_cards < number_of_cards:
             raise ValueError(
