42f90807ff71e5c7c6f4a8e503177f2cbe39f826
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-30 17:35:13 -0300

    :sparkles: Add the only one in assertion

--- tests/assertions/assertions.py
+++ tests/assertions/assertions.py
@@ -34,3 +34,15 @@ def assert_list_raises_not_minimum_length(attr_name: str, minimum_length: int, c
     with raises(ValueError) as exception_info:
         closure(invalid_list)
     assert str(exception_info.value) == f"Length of '{attr_name}' must be => {minimum_length}: {len(invalid_list)}"
+
+
+def with_the_only_one_in(collection, closure):
+    assert len(collection) == 1
+    closure(collection[0])
+
+
+def assert_the_only_one_in(collection, expected_element):
+    def assert_equals(element, another_element):
+        assert element == another_element
+
+    with_the_only_one_in(collection, lambda found_element: assert_equals(found_element, expected_element))
