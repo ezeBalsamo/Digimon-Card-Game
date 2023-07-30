7d8404c45f963a4e1319328a00adb8fe92a6ab29
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-30 02:17:58 -0300

    :sparkles: Add raises not within range assertion

--- tests/assertions/assertions.py
+++ tests/assertions/assertions.py
@@ -13,3 +13,10 @@ def assert_raises_not_blank(attr_name: str, closure: Callable[[str], Any]):
         with raises(ValueError) as exception_info:
             closure(invalid_value)
         assert str(exception_info.value) == f'{attr_name} must not be blank.'
+
+
+def assert_raises_not_within_range(attr_name: str, lower: int, upper: int, closure: Callable[[int], Any]):
+    for invalid_value in [lower - 1, upper + 1]:
+        with raises(ValueError) as exception_info:
+            closure(invalid_value)
+        assert str(exception_info.value) == f'{attr_name} must be between {lower} and {upper}.'
