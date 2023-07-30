b662402b6b2e2266a8453c44677babe7c493c576
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-30 17:21:00 -0300

    :recycle: Add assert can draw many assertion

--- src/digimon_card_game/deck.py
+++ src/digimon_card_game/deck.py
@@ -13,13 +13,16 @@ class Deck:
 
     def draw_many(self, number_of_cards):
         self.assert_is_not_empty()
+        self.assert_can_draw_many(number_of_cards)
+        drawn_cards = self.cards[:number_of_cards]
+        del self.cards[:number_of_cards]
+        return drawn_cards
+
+    def assert_can_draw_many(self, number_of_cards):
         number_of_remaining_cards = len(self.cards)
         if number_of_remaining_cards < number_of_cards:
             raise ValueError(
                 f'You cannot draw {number_of_cards} cards. Number of remaining cards: {number_of_remaining_cards}.')
-        drawn_cards = self.cards[:number_of_cards]
-        del self.cards[:number_of_cards]
-        return drawn_cards
 
     def assert_is_not_empty(self):
         if self.is_empty():
