a31db869df93f9d15dc302034f67d353a8784f9e
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-30 02:25:23 -0300

    :sparkles: Add digimon card

--- src/digimon_card_game/__init__.py
+++ src/digimon_card_game/__init__.py
@@ -4,3 +4,4 @@ from .digimon_form import DigimonForm
 from .digimon_attribute import DigimonAttribute
 from .digimon_type import DigimonType
 from .digi_egg_card import DigiEggCard
+from .digimon_card import DigimonCard
