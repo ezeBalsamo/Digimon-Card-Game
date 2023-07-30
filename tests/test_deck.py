cf2b383af486cf8eab169bca29135b8137a7e4e9
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-30 15:21:01 -0300

    :white_check_mark: Add test: deck instance creation and accessing

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -1,4 +1,4 @@
-from src.digimon_card_game import Deck
+from src.digimon_card_game import Deck, DigiEggCard, CardColor, CardRarity, DigimonType
 from pytest import raises
 
 
@@ -8,3 +8,11 @@ def test_01_cannot_create_deck_without_cards():
     with raises(ValueError) as exception_info:
         Deck(cards=invalid_cards)
     assert str(exception_info.value) == f"Length of 'cards' must be => {minimum_length}: {len(invalid_cards)}"
+
+
+def test_02_instance_creation_and_accessing():
+    card = DigiEggCard(name='Koromon', color=CardColor.RED, identifier='ST1-01', rarity=CardRarity.COMMON,
+                       type=DigimonType.LESSER, level=2)
+    cards = [card]
+    deck = Deck(cards=cards)
+    assert deck.cards == cards
