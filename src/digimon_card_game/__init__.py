0bad6e22e7d75154138e09520a9e6bb913984b2a
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-30 02:06:51 -0300

    :sparkles: Add card rarity

--- src/digimon_card_game/__init__.py
+++ src/digimon_card_game/__init__.py
@@ -1,4 +1,5 @@
 from .card_color import CardColor
+from .card_rarity import Rarity, CardRarity
 from .digimon_form import DigimonForm
 from .digimon_attribute import DigimonAttribute
 from .digimon_type import DigimonType
