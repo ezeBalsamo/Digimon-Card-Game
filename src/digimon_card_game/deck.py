7384497ca6f04d3c6fa8fbfd9966752c5a685c2f
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-30 17:13:09 -0300

    :recycle: Make Deck class frozen again

--- src/digimon_card_game/deck.py
+++ src/digimon_card_game/deck.py
@@ -1,9 +1,9 @@
-from attr import define, field
+from attr import frozen, field
 from src.digimon_card_game import Card
 from attr.validators import min_len
 
 
-@define(kw_only=True)
+@frozen(kw_only=True)
 class Deck:
     cards: list[Card] = field(validator=min_len(1))
 
