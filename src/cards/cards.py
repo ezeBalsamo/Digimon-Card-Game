fabe864f3ad798ac0325d7ab38a6b0b471341e55
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-30 17:21:30 -0300

    :sparkles: Create cards package

---
+++ src/cards/cards.py
@@ -0,0 +1,17 @@
+from src.digimon_card_game import DigiEggCard, CardColor, CardRarity, DigimonType, OptionCard, DigimonCard, \
+    DigimonForm, DigimonAttribute
+
+
+def koromon():
+    return DigiEggCard(name='Koromon', color=CardColor.RED, identifier='ST1-01', rarity=CardRarity.COMMON,
+                       type=DigimonType.LESSER, level=2)
+
+
+def shadow_wing():
+    return OptionCard(name='Shadow Wing', color=CardColor.RED, identifier='ST1-13', rarity=CardRarity.COMMON, cost=1)
+
+
+def biyomon():
+    return DigimonCard(name='Biyomon', color=CardColor.RED, identifier='ST1-02',
+                       rarity=CardRarity.COMMON, form=DigimonForm.ROOKIE, attribute=DigimonAttribute.VACCINE,
+                       type=DigimonType.BIRD, cost=2, power=3000, level=3)
