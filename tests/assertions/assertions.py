6f1a21e006faeebcf49c144f9fde3ef070bd8509
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-07-30 02:17:09 -0300

    :sparkles: Add raises not blank assertion

--- tests/assertions/assertions.py
+++ tests/assertions/assertions.py
@@ -1,7 +1,15 @@
 from enum import Enum
-from typing import Any, Type, Set
+from typing import Any, Type, Set, Callable
+from pytest import raises
 
 
 def assert_expected_enum_values(enum_class: Type[Enum], expected_enum_values: Set[Any]):
     enum_values = set(map(lambda enum_item: enum_item.value, list(enum_class)))
     assert enum_values == expected_enum_values
+
+
+def assert_raises_not_blank(attr_name: str, closure: Callable[[str], Any]):
+    for invalid_value in ['', ' ']:
+        with raises(ValueError) as exception_info:
+            closure(invalid_value)
+        assert str(exception_info.value) == f'{attr_name} must not be blank.'
