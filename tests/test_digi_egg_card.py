e60852277d235c74f6183b2cd8b93982bfd510fa
FedericoFuentesWeber <fedefuentesweber@frba.utn.edu.ar>
2023-08-03 20:40:25 -0300

    :pencil2: Fix Koromon card rarity from common to uncommon

--- tests/test_digi_egg_card.py
+++ tests/test_digi_egg_card.py
@@ -5,7 +5,7 @@ from tests.assertions.attributes import assert_attr_raises_not_blank, assert_att
 def test_01_name_must_not_be_blank():
     assert_attr_raises_not_blank('name',
                                  lambda invalid_name: DigiEggCard(name=invalid_name, color=CardColor.RED,
-                                                                  identifier='ST1-01', rarity=CardRarity.COMMON,
+                                                                  identifier='ST1-01', rarity=CardRarity.UNCOMMON,
                                                                   type=DigimonType.LESSER, level=2))
 
 
@@ -13,7 +13,7 @@ def test_02_identifier_must_not_be_blank():
     assert_attr_raises_not_blank('identifier',
                                  lambda invalid_identifier: DigiEggCard(name='Koromon', color=CardColor.RED,
                                                                         identifier=invalid_identifier,
-                                                                        rarity=CardRarity.COMMON,
+                                                                        rarity=CardRarity.UNCOMMON,
                                                                         type=DigimonType.LESSER,
                                                                         level=2))
 
@@ -22,24 +22,25 @@ def test_03_level_must_be_between_two_and_seven():
     assert_attr_raises_not_within_range('level', 2, 7,
                                         lambda invalid_level: DigiEggCard(name='Koromon', color=CardColor.RED,
                                                                           identifier='ST1-01',
-                                                                          rarity=CardRarity.COMMON,
+                                                                          rarity=CardRarity.UNCOMMON,
                                                                           type=DigimonType.LESSER,
                                                                           level=invalid_level))
 
 
 def test_04_instance_creation_and_accessing():
-    card = DigiEggCard(name='Koromon', color=CardColor.RED, identifier='ST1-01', rarity=CardRarity.COMMON,
+    card = DigiEggCard(name='Koromon', color=CardColor.RED, identifier='ST1-01', rarity=CardRarity.UNCOMMON,
                        type=DigimonType.LESSER, level=2)
     assert card.name == 'Koromon'
     assert card.color == CardColor.RED
     assert card.identifier == 'ST1-01'
-    assert card.rarity == CardRarity.COMMON
+    assert card.rarity == CardRarity.UNCOMMON
     assert card.form == DigimonForm.IN_TRAINING
     assert card.type == DigimonType.LESSER
     assert card.level == 2
 
 
 def test_05_instance_creation_without_level():
-    card_without_level = DigiEggCard(name='Koromon', color=CardColor.RED, identifier='ST1-01', rarity=CardRarity.COMMON,
+    card_without_level = DigiEggCard(name='Koromon', color=CardColor.RED, identifier='ST1-01',
+                                     rarity=CardRarity.UNCOMMON,
                                      type=DigimonType.LESSER)
     assert card_without_level.level is None
