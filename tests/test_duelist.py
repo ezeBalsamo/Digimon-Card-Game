e13fa102f070e45a058d5dd2770ab324981b4f91
FedericoFuentesWeber <ffuentesweber@gmail.com>
2023-08-04 19:19:44 -0300

    :white_check_mark: Add test instance creation and accessing

--- tests/test_duelist.py
+++ tests/test_duelist.py
@@ -8,3 +8,10 @@ def test_01_cannot_create_duelist_with_blank_name():
     assert_attr_raises_not_blank('name',
                                  lambda invalid_name: Duelist(name=invalid_name,
                                                               deckSet=Deckset(main_deck=Deck(cards=[shadow_wing()]))))
+
+
+def test_02_instance_creation_and_accessing():
+    duelist = Duelist(name='Pepe Sand',
+                      deckSet=Deckset(main_deck=Deck(cards=[shadow_wing()])))
+    assert duelist.name == 'Pepe Sand'
+    assert duelist.deckSet == Deckset(main_deck=Deck(cards=[shadow_wing()]))
