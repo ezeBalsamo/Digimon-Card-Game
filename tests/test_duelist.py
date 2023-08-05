3dcf2f76c164ecb8ae648313fb6546dbd569574d
FedericoFuentesWeber <ffuentesweber@gmail.com>
2023-08-05 15:53:15 -0300

    :white_check_mark: Define function deckset

--- tests/test_duelist.py
+++ tests/test_duelist.py
@@ -4,6 +4,10 @@ from duels import Duelist
 from tests.assertions.attributes import assert_attr_raises_not_blank
 
 
+def deckset():
+    return Deckset(main_deck=Deck(cards=[shadow_wing()]))
+
+
 def test_01_cannot_create_duelist_with_blank_name():
     assert_attr_raises_not_blank('name',
                                  lambda invalid_name: Duelist(name=invalid_name,
@@ -12,9 +16,8 @@ def test_01_cannot_create_duelist_with_blank_name():
 
 def test_02_instance_creation_and_accessing():
     name = 'Pepe Sand'
-    deck = Deck(cards=[shadow_wing()])
-    deckset = Deckset(main_deck=deck)
+    the_deckset = deckset()
     duelist = Duelist(name=name,
-                      deckset=deckset)
+                      deckset=the_deckset)
     assert duelist.name == name
-    assert duelist.deckset == deckset
+    assert duelist.deckset == the_deckset
