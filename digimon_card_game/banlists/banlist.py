31c8ae1f080042551643d583b235f1153806d612
FedericoFuentesWeber <ffuentesweber@gmail.com>
2023-08-14 23:02:01 -0300

    :white_check_mark: Add test cannot create banlist with negative number of cards

--- digimon_card_game/banlists/banlist.py
+++ digimon_card_game/banlists/banlist.py
@@ -1,10 +1,17 @@
-from attr import frozen, field
+from typing import Any
+from attr import frozen, field, Attribute
 from ..cards import Card
 from attrs.validators import min_len
 from datetime import date
 
 
+def validate_non_negative_number_of_copies(_instance: Any, attribute: Attribute,
+                                           number_of_copies_by_card: dict[Card, int]):
+    if any(value < 0 for value in number_of_copies_by_card.values()):
+        raise ValueError(f"{attribute.name} must not include negative values.")
+
+
 @frozen(kw_only=True)
 class Banlist:
     date: date
-    number_of_copies_by_card: dict[Card, int] = field(validator=min_len(1))
+    number_of_copies_by_card: dict[Card, int] = field(validator=[min_len(1), validate_non_negative_number_of_copies])
--- tests/test_banlist.py
+++ tests/test_banlist.py
@@ -2,6 +2,7 @@ from datetime import date
 from digimon_card_game.banlists.banlist import Banlist
 from digimon_card_game.cards import shadow_wing
 from tests.assertions.dictionary.assertions import assert_dict_raises_not_minimum_length
+from pytest import raises
 
 
 def test_01_cannot_create_banlist_without_cards():
@@ -11,7 +12,16 @@ def test_01_cannot_create_banlist_without_cards():
                                               number_of_copies_by_card=invalid_cards))
 
 
-def test_02_instance_creation_and_accessing():
+def test_02_cannot_create_banlist_with_negative_number_of_cards():
+    today = date.today()
+    invalid_number_of_copies_by_card = {shadow_wing(): -1}
+
+    with raises(ValueError) as exception_info:
+        Banlist(date=today, number_of_copies_by_card=invalid_number_of_copies_by_card)
+    assert str(exception_info.value) == 'number_of_copies_by_card must not include negative values.'
+
+
+def test_03_instance_creation_and_accessing():
     today = date.today()
     number_of_copies_by_card = {shadow_wing(): 1}
     banlist = Banlist(date=today, number_of_copies_by_card=number_of_copies_by_card)
