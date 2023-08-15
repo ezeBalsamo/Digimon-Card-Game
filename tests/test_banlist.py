5d2d4450a42dbb83beafa94fc81f757bb8ef7f5f
FedericoFuentesWeber <ffuentesweber@gmail.com>
2023-08-14 21:41:43 -0300

    :white_check_mark: Add test banlist dictionary must have at least one key

--- tests/test_banlist.py
+++ tests/test_banlist.py
@@ -1,7 +1,17 @@
 from digimon_card_game.banlists.banlist import Banlist
 from tests.assertions import assert_attr_raises_not_blank
+from tests.assertions.dictionary.assertions import assert_dict_raises_not_minimum_length
 
 
 def test_01_name_must_not_be_blank():
     assert_attr_raises_not_blank('name',
-                                 lambda invalid_name: Banlist(name=invalid_name))
+                                 lambda invalid_name: Banlist(
+                                     name=invalid_name,
+                                     number_of_copies_by_card={}))
+
+
+def test_02_cannot_create_banlist_without_cards():
+    assert_dict_raises_not_minimum_length('number_of_copies_by_card', 1,
+                                          lambda invalid_cards: Banlist(
+                                              name="Banlist",
+                                              number_of_copies_by_card=invalid_cards))
