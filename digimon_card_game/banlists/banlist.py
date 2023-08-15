d2c047ca7385c37e9639add72b4ad0f1c3b7d35b
FedericoFuentesWeber <ffuentesweber@gmail.com>
2023-08-14 22:18:19 -0300

    :white_check_mark: Add test instance creation and accessing for banlist

--- digimon_card_game/banlists/banlist.py
+++ digimon_card_game/banlists/banlist.py
@@ -1,8 +1,10 @@
 from attr import frozen, field
 from ..cards import Card
 from attrs.validators import min_len
+from datetime import date
 
 
 @frozen(kw_only=True)
 class Banlist:
+    date: date
     number_of_copies_by_card: dict[Card, int] = field(validator=min_len(1))
--- tests/test_banlist.py
+++ tests/test_banlist.py
@@ -1,7 +1,20 @@
+from datetime import date
 from digimon_card_game.banlists.banlist import Banlist
+from digimon_card_game.cards import shadow_wing
 from tests.assertions.dictionary.assertions import assert_dict_raises_not_minimum_length
 
 
 def test_01_cannot_create_banlist_without_cards():
     assert_dict_raises_not_minimum_length('number_of_copies_by_card', 1,
-                                          lambda invalid_cards: Banlist(number_of_copies_by_card=invalid_cards))
+                                          lambda invalid_cards: Banlist(
+                                              date=date.today(),
+                                              number_of_copies_by_card=invalid_cards))
+
+
+def test_02_instance_creation_and_accessing():
+    today = date.today()
+    number_of_copies_by_card = {shadow_wing(): 1}
+    banlist = Banlist(date=today, number_of_copies_by_card=number_of_copies_by_card)
+
+    assert banlist.date == today
+    assert banlist.number_of_copies_by_card == number_of_copies_by_card
