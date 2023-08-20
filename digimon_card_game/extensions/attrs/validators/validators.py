345a6b2d03d27f1f7042f3fe0b2c5fb2e7e54683
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-08-20 18:12:21 -0300

    :bug: fix tests failing due to TypeError: type 'Attribute' is not subscriptable

--- digimon_card_game/extensions/attrs/validators/validators.py
+++ digimon_card_game/extensions/attrs/validators/validators.py
@@ -1,7 +1,6 @@
+from __future__ import annotations
 from typing import Any, Callable
 from attrs import Attribute
-
-WithinRangeValidator = Callable[[Any, Attribute[int], int], None]
 
 
 def not_blank(_instance: Any, attribute: Attribute[str], value: str) -> None:
@@ -9,7 +8,7 @@ def not_blank(_instance: Any, attribute: Attribute[str], value: str) -> None:
         raise ValueError(f'{attribute.name} must not be blank.')
 
 
-def within_range(lower: int, upper: int) -> WithinRangeValidator:
+def within_range(lower: int, upper: int) -> Callable[[Any, Attribute[int], int], None]:
     def wrapper(_instance: Any, attribute: Attribute[int], value: int) -> None:
         if not lower <= value <= upper:
             raise ValueError(f'{attribute.name} must be between {lower} and {upper}.')
