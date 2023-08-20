4189357b7e0ab4f3bbfd8bde17f15d26ab46ab3b
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-08-20 17:07:07 -0300

    :rotating_light: pass mypy --strict --implicit-reexport in digimon_card_game/extensions

--- digimon_card_game/extensions/attrs/validators/validators.py
+++ digimon_card_game/extensions/attrs/validators/validators.py
@@ -1,14 +1,16 @@
-from typing import Any
+from typing import Any, Callable
 from attrs import Attribute
 
+WithinRangeValidator = Callable[[Any, Attribute[int], int], None]
 
-def not_blank(_instance: Any, attribute: Attribute, value: str):
+
+def not_blank(_instance: Any, attribute: Attribute[str], value: str) -> None:
     if value.strip() == "":
         raise ValueError(f'{attribute.name} must not be blank.')
 
 
-def within_range(lower: int, upper: int):
-    def wrapper(_instance: Any, attribute: Attribute, value: int):
+def within_range(lower: int, upper: int) -> WithinRangeValidator:
+    def wrapper(_instance: Any, attribute: Attribute[int], value: int) -> None:
         if not lower <= value <= upper:
             raise ValueError(f'{attribute.name} must be between {lower} and {upper}.')
 
