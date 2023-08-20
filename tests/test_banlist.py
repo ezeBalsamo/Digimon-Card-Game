f5051d9390d9d1d5c66e11ffdfe1c84cb751634c
FedericoFuentesWeber <ffuentesweber@gmail.com>
2023-08-20 18:47:25 -0300

    :recycle: Assigned shadow_wing() to a variable in test_banlist
    Signed-off-by: FedericoFuentesWeber <ffuentesweber@gmail.com>

--- tests/test_banlist.py
+++ tests/test_banlist.py
@@ -6,6 +6,9 @@ from .assertions import assert_dict_raises_not_minimum_length
 from pytest import raises
 
 
+shadow_wing = shadow_wing()
+
+
 def test_01_cannot_create_banlist_without_cards():
     assert_dict_raises_not_minimum_length('number_of_copies_by_card', 1,
                                           lambda invalid_cards: Banlist(
@@ -15,7 +18,7 @@ def test_01_cannot_create_banlist_without_cards():
 
 def test_02_cannot_create_banlist_with_negative_number_of_cards():
     today = date.today()
-    invalid_number_of_copies_by_card = {shadow_wing(): -1}
+    invalid_number_of_copies_by_card = {shadow_wing: -1}
 
     with raises(ValueError) as exception_info:
         Banlist(date=today, number_of_copies_by_card=invalid_number_of_copies_by_card)
@@ -25,7 +28,7 @@ def test_02_cannot_create_banlist_with_negative_number_of_cards():
 def test_03_instance_creation_and_accessing():
     today = date.today()
     number_of_copies_by_card = {
-        shadow_wing(): 3,
+        shadow_wing: 3,
         biyomon(): 0
     }
     banlist = Banlist(date=today, number_of_copies_by_card=number_of_copies_by_card)
@@ -38,21 +41,21 @@ def test_04_card_is_allowed_when_not_in_the_banlist():
     number_of_copies_by_card = {biyomon(): 0}
     banlist = Banlist(date=date.today(), number_of_copies_by_card=number_of_copies_by_card)
 
-    assert banlist.is_card_allowed(shadow_wing(), 1)
+    assert banlist.is_card_allowed(shadow_wing, 1)
 
 
 def test_05_card_is_allowed_when_is_in_the_banlist_but_number_of_copies_is_lower_than_the_allowed():
-    number_of_copies_by_card = {shadow_wing(): 3}
+    number_of_copies_by_card = {shadow_wing: 3}
     banlist = Banlist(date=date.today(), number_of_copies_by_card=number_of_copies_by_card)
 
-    assert banlist.is_card_allowed(shadow_wing(), 1)
+    assert banlist.is_card_allowed(shadow_wing, 1)
 
 
 def test_06_card_is_allowed_when_is_in_the_banlist_but_number_of_copies_equals_the_allowed():
-    number_of_copies_by_card = {shadow_wing(): 3}
+    number_of_copies_by_card = {shadow_wing: 3}
     banlist = Banlist(date=date.today(), number_of_copies_by_card=number_of_copies_by_card)
 
-    assert banlist.is_card_allowed(shadow_wing(), 3)
+    assert banlist.is_card_allowed(shadow_wing, 3)
 
 
 def test_07_card_is_not_allowed_when_is_in_the_banlist_but_number_of_copies_is_greater_than_the_allowed():
@@ -64,18 +67,18 @@ def test_07_card_is_not_allowed_when_is_in_the_banlist_but_number_of_copies_is_g
 
 def test_08_deck_is_allowed_when_all_cards_are_allowed():
     number_of_copies_by_card = {
-        shadow_wing(): 3,
+        shadow_wing: 3,
         biyomon(): 0
     }
     banlist = Banlist(date=date.today(), number_of_copies_by_card=number_of_copies_by_card)
-    deck = Deck(cards=[shadow_wing()])
+    deck = Deck(cards=[shadow_wing])
 
     assert banlist.is_deck_allowed(deck) is True
 
 
 def test_09_deck_is_not_allowed_when_at_least_one_card_is_not_allowed():
     number_of_copies_by_card = {
-        shadow_wing(): 3,
+        shadow_wing: 3,
         biyomon(): 0
     }
     banlist = Banlist(date=date.today(), number_of_copies_by_card=number_of_copies_by_card)
@@ -95,11 +98,11 @@ def test_10_deckset_is_not_allowed_if_main_deck_is_not_allowed():
 
 def test_11_deckset_is_not_allowed_if_at_least_one_the_optional_decks_is_not_allowed():
     number_of_copies_by_card = {
-        shadow_wing(): 3,
+        shadow_wing: 3,
         biyomon(): 0
     }
     banlist = Banlist(date=date.today(), number_of_copies_by_card=number_of_copies_by_card)
-    main_deck = Deck(cards=[shadow_wing()])
+    main_deck = Deck(cards=[shadow_wing])
     biyomon_deck = Deck(cards=[biyomon()])
     optional_decks = {'Biyomon-Deck': biyomon_deck}
     deckset = Deckset(name="Deckset", main_deck=main_deck, optional_decks=optional_decks)
@@ -109,11 +112,11 @@ def test_11_deckset_is_not_allowed_if_at_least_one_the_optional_decks_is_not_all
 
 def test_12_deckset_is_allowed_if_all_decks_are_allowed():
     number_of_copies_by_card = {
-        shadow_wing(): 3,
+        shadow_wing: 3,
         biyomon(): 0
     }
     banlist = Banlist(date=date.today(), number_of_copies_by_card=number_of_copies_by_card)
-    main_deck = Deck(cards=[shadow_wing()])
+    main_deck = Deck(cards=[shadow_wing])
     digi_egg_deck = Deck(cards=[koromon()])
     optional_decks = {'Digi-Egg': digi_egg_deck}
     deckset = Deckset(name="Deckset", main_deck=main_deck, optional_decks=optional_decks)
