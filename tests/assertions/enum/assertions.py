7624160698254d6fb502e4280c1cdad0b55a652d
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2023-08-20 17:12:46 -0300

    :rotating_light: pass mypy --strict --implicit-reexport in tests/assertions

--- tests/assertions/enum/assertions.py
+++ tests/assertions/enum/assertions.py
@@ -2,6 +2,6 @@ from enum import Enum
 from typing import Type, Set, Any
 
 
-def assert_expected_enum_values(enum_class: Type[Enum], expected_enum_values: Set[Any]):
+def assert_expected_enum_values(enum_class: Type[Enum], expected_enum_values: Set[Any]) -> None:
     enum_values = set(map(lambda enum_item: enum_item.value, list(enum_class)))
     assert enum_values == expected_enum_values
--- tests/assertions/number/assertions.py
+++ tests/assertions/number/assertions.py
@@ -2,11 +2,12 @@ from typing import Any, Callable
 from pytest import raises
 
 
-def assert_raises_not_positive(closure: Callable[[int], Any], explanation: str):
+def assert_raises_not_positive(closure: Callable[[int], Any], explanation: str) -> None:
     assert_raises_not_positive_because(closure, lambda _: explanation)
 
 
-def assert_raises_not_positive_because(closure: Callable[[int], Any], explanation_closure: Callable[[int], str]):
+def assert_raises_not_positive_because(closure: Callable[[int], Any],
+                                       explanation_closure: Callable[[int], str]) -> None:
     for invalid_value in [-1, 0]:
         with raises(ValueError) as exception_info:
             closure(invalid_value)
