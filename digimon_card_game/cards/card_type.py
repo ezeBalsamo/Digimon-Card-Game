35e8f9443dd890133fac99ed9ee547daa6221bdf
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2024-07-21 13:06:21 -0300

    :construction_worker: CI enhancements
    - fix ruff lint warnings
    - update ruff version to 0.5.4
    - execute tests workflow on push
    - remove python-version 3.11 from matrix

--- digimon_card_game/cards/card_type.py
+++ digimon_card_game/cards/card_type.py
@@ -1,8 +1,6 @@
-from typing import TypeAlias
-
 from .digi_egg_card import DigiEggCard
 from .digimon_card import DigimonCard
 from .option_card import OptionCard
 from .tamer_card import TamerCard
 
-Card: TypeAlias = DigiEggCard | DigimonCard | TamerCard | OptionCard
+type Card = DigiEggCard | DigimonCard | TamerCard | OptionCard
